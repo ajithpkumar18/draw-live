245d6f65398044c050ed5333910625c3cc9fb0b3
Ajith <ajithpkumar231@gmail.com>
2025-06-15 22:06:16 +0530

    ws url, token verifies

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,39 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import { JWT_SECRET } from "@repo/backend-common/config"
+import jwt from "jsonwebtoken"
+import { CreateRoomSchema, CreateUserSchema } from "@repo/common/types"
 
 const app = express()
 
 app.get("/", (req, res) => {
     res.status(200).json("This is the server")
+})
+
+app.get("/signin", (req, res) => {
+    const userID = 1;
+    const token = jwt.sign({ userID }, JWT_SECRET)
+    res.status(200).json(token)
+})
+
+app.get("/signup", (req: Request, res: Response) => {
+
+    const data = CreateUserSchema.safeParse(req.body)
+
+    if (!data.success) {
+        res.json({ message: "Incorrect inputs" })
+        return
+    }
+    res.status(200).json({ userID: "123" })
+})
+
+app.get("/room", (req, res) => {
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({ message: "Incorrect Inputs" })
+        return
+    }
+
+    res.status(200).json({ roomID: "123" })
 })
 
 app.listen(3004, () => {
